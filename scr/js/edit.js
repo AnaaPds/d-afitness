73f00673edeb45dd7846b3ac7ee31f10f0f30e3e
AnaaPds <anapauladantas1804@gmail.com>
2024-12-04 08:13:03 -0300

    Update edit.js

--- scr/js/edit.js
+++ scr/js/edit.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // Obtém os parâmetros da URL
+    
     const params = new URLSearchParams(window.location.search);
     const id = params.get("id");
     const name = params.get("name");
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const email = params.get("email");
     const modality = params.get("modality");
 
-    // Preenche os campos do formulário com os dados recebidos
+    
     const nameInput = document.getElementById("name");
     const ageInput = document.getElementById("age");
     const emailInput = document.getElementById("email");
@@ -20,20 +20,20 @@ document.addEventListener("DOMContentLoaded", () => {
         modalityInput.value = modality;
     }
 
-    // Botão Cancelar
+    
     const cancelButton = document.querySelector(".cancel-button");
     cancelButton.addEventListener("click", () => {
-        // Retorna para a página inicial
+        
         window.location.href = "index.html";
     });
 
-    // Botão Salvar
+    
     const saveButton = document.querySelector(".btn-save");
     saveButton.addEventListener("click", (event) => {
-        event.preventDefault(); // Impede o comportamento padrão do botão
+        event.preventDefault(); 
 
-        // Aqui você pode implementar o envio dos dados para o servidor
+        
         alert("Alterações salvas com sucesso!");
-        window.location.href = "index.html"; // Retorna à página inicial após salvar
+        window.location.href = "index.html"; 
     });
 });
